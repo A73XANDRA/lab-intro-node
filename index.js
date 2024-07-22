ec628b3913815d349e72547a6b38726f6e285db2
Alex Morel <alex092697@outlook.es>
2024-07-22 16:30:35 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,71 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
 
-  max() {}
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
 
-  min() {}
+  }
 
-  sum() {}
+  get(pos) {
 
-  avg() {}
+    if (this.items[pos] === undefined) {
+      throw new Error("OutOfBounds");
+    }
+
+    return this.items[pos]
+  }
+
+  max() {
+
+
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return Math.max(...this.items)
+
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    }
+
+    return Math.min(...this.items)
+
+  }
+
+  sum() {
+
+
+    const sum = this.items.reduce((acc, curValue) => {
+      return acc + curValue
+    }, 0)
+
+    return sum
+  }
+
+
+  avg() {
+
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    }
+
+    let AvgNumber = 0;
+
+    AvgNumber = this.sum() / this.length
+
+    return AvgNumber
+
+  }
 }
 
 module.exports = SortedList;
